351dc6a6a19f04aa84453406a09b5122be2ef9a2
Aditya Choudhary <adityachoudhary1980@gmail.com>
2022-02-01 15:00:11 -0800

    update

--- src/components/Header/Categorywise.js
+++ src/components/Header/Categorywise.js
@@ -2,10 +2,14 @@ import React from 'react';
 import './Category.css';
 
 const Categorywise = (props) => {
+    function handleClick(e){
+        e.preventDefault();
+        e.target.classList.add('active');
+    }
     return (
         <div className="item-container ">
             <a href="/" className="item-link">
-                <div className="item ">
+                <div onClick={handleClick} className="item">
                     <li className='list-item'>{props.name}</li>
                 </div>
             </a>
